766940701c34c85d49103d13a8a07fb772b4cb7e
Bill Mårtensson <bill@magictechnology.se>
2021-10-11 11:32:45 +0200

    11okt

--- piaxv5firebase/ViewController.swift
+++ piaxv5firebase/ViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var addfruitTextfield: UITextField!
+    
+    @IBOutlet weak var fruitTableview: UITableView!
     
     var fruits = [String]()
 
@@ -15,11 +20,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        addfruitTextfield.delegate = self
+        
         fruits.append("Banan")
         fruits.append("Apelsin")
         fruits.append("Kiwi")
         
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if(textField == addfruitTextfield)
+        {
+            letsAddStuff()
+        }
+        
+        
+        return true
+    }
+    
+    @IBAction func addFruit(_ sender: Any) {
+        letsAddStuff()
+    }
+    
+    func letsAddStuff() {
+        if(addfruitTextfield.text != "")
+        {
+            fruits.append(addfruitTextfield.text!)
+            
+            fruitTableview.reloadData()
+            
+            addfruitTextfield.text = ""
+        }
+    }
+    
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fruits.count
@@ -36,7 +71,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
         
         print("Klickat på en rad " + String(indexPath.row))
         
